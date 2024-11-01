af3800ad2a620a0fda57fd7972f9bff03b6dbe2a
Sam-Rags <168376297+Sam-Rags@users.noreply.github.com>
2024-11-01 15:57:56 -0400

    output demonstration the function of gpacheck

---
+++ sragsdale_gpacheck_sdev220_outputs.py
@@ -0,0 +1,22 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Enter the student's last name: Ragsdale
+... Enter the student's first name: Samuel
+... Enter the student's GPA: 3.6
+... Congratulations, Samuel Ragsdale has made the Dean's List!
+... Do you want to enter another student? (y/n)y
+... Enter the student's last name: Reynolds
+... Enter the student's first name: Burt
+... Enter the student's GPA: 3.25
+... Congratulations, Burt Reynolds has made the Honor Roll!
+... Do you want to enter another student? (y/n)y
+... Enter the student's last name: Norris
+... Enter the student's first name: Chuck
+... Enter the student's GPA: 2.6
+... Do you want to enter another student? (y/n)y
+... Enter the student's last name: Hardy
+... Enter the student's first name: Ed
+... Enter the student's GPA: 4.0
+... Congratulations, Ed Hardy has made the Dean's List!
+... Do you want to enter another student? (y/n)y
+... Enter the student's last name: ZZZ  
