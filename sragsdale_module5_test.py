abd5f58c4920d14f1897c9ef40004f3eb7efa179
Sam-Rags <168376297+Sam-Rags@users.noreply.github.com>
2024-11-22 16:09:09 -0500

    Update sragsdale_module5_test.py

--- sragsdale_module5_test.py
+++ sragsdale_module5_test.py
@@ -1,5 +1,9 @@
 import unittest
 
+from my_sum import sum
+
+from fractions import Fraction
+
 class TestSum(unittest.TestCase):
     def test_list_int(self):
         '''
@@ -8,6 +12,14 @@ class TestSum(unittest.TestCase):
         data = [1, 2, 3]
         result = sum(data)
         self.assertEqual(result, 6)
+
+    def test_list_fractions(self):
+        '''
+        Test that it can sum a list of fractions
+        '''
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
 
 if __name__ == '__main__':
     unittest.main()
